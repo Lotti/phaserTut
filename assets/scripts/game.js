d3fcb69f69531b33b5a67da44f340cef3e015b40
Lotti <lotti86@gmail.com>
2014-03-21 09:14:41 +0100

    updated to phaser 2.0

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,31 +1,28 @@
 var debug = true;
 var gameDiv = "game";
-var basketColors = ['box','boxgreen','boxred'];
+var basketColors = ['basket','basketgreen','basketred'];
 var colors = ['yellow','red','green','blue','purple','grey'];
 var dots;
 var dotCG;
-var dotMaterial;
 var circle;
 var circleCG;
-var circleMaterial;
 var baskets;
 var basketCG;
-var basketMaterial;
 var fps;
 var points;
+var started = false;
 
 var game = new Phaser.Game($("#"+gameDiv).width(), $("#"+gameDiv).height(), debug ? Phaser.CANVAS : Phaser.AUTO, gameDiv, { preload: preload, loadUpdate: loadUpdate, loadRender: loadRender, create: create, update: update, render: render });
 
 function preload() {
-	game.load.image('boxgreen','assets/sprites/boxgreen.png');
-	game.load.image('boxred','assets/sprites/boxred.png');
 	game.load.image('circle', 'assets/sprites/cd.png');	
 	for(var i in colors) {
 		game.load.image(colors[i]+'Dot', 'assets/sprites/'+colors[i]+'Dot.png');
 	}
 	for(var i in basketColors) {
 		game.load.image(basketColors[i],'assets/sprites/'+basketColors[i]+'.png');
-	}	
+	}
+	game.load.physics('physicsBasket', 'assets/physics/basket.json');
 }
 
 function loadUpdate() {
@@ -44,7 +41,8 @@ function create() {
 			fps.setText(game.time.fps+' fps');
 		}
 	//}
-		
+	
+	
 	game.time.advancedTiming = true;
 	points = game.add.text(game.world.width-5, 2.5, '0 points', { font: '30px Verdana', fill: '#FFFFFF', align: 'left' });
 	points.p = 0;
@@ -54,36 +52,42 @@ function create() {
 		points.setText(points.p+' points');
 	}		
 
-	game.physics.gravity.y = 200;
-	game.physics.restitution = 0.6;
-	game.physics.friction = 0.8;		
+	cursors = game.input.keyboard.createCursorKeys();
+	
+	game.physics.startSystem(Phaser.Physics.P2JS);
+	
+	game.physics.p2.gravity.y = 100;
+	game.physics.p2.defaultRestitution = 0.8;
+	game.physics.p2.defaultFriction = 0.8;		
 
 	//circle
+	circleCG = game.physics.p2.createCollisionGroup();
 	
-	circleMaterial = game.physics.createMaterial('circleMaterial');
-	circleCG = game.physics.createCollisionGroup();
-
-	circle = game.add.sprite(150, 15, 'circle');
+	circle = game.add.sprite(150, 30, 'circle');
 	circle.collidedWith = [];
 	circle.name = 'circle';
 	circle.anchor.setTo(0.5,0.5);
 	circle.scale.setTo(0.5,0.5);
-	circle.physicsEnabled = true;
+	game.physics.p2.enable(circle, debug);
+	circle.body.data.gravityScale = 1;
 	circle.body.setCircle(circle.width * .5);
-	circle.body.mass = 4;
-	circle.body.damping = 0.2; //bounce?
-	circle.body.setMaterial(circleMaterial);
+	circle.body.damping = 0.2; //velocity lost per second
+	circle.body.mass = 1;
 	circle.body.setCollisionGroup(circleCG);
-	//circle.body.static = true;
+	circle.body.data.motionState = 2; //circle.body.static = true;
 	circle.body.collideWorldBounds = true;
 
-	//dots
-	dotCG = game.physics.createCollisionGroup();		
+	//circle.body.setZeroDamping();
+	//circle.body.fixedRotation = true;	
 
-	var rows = 6;
+	
+	//dots
+	dotCG = game.physics.p2.createCollisionGroup();		
+
+	var rows = 7;
 	var cols = 20;
 	var startX = 50;
-	var startY = 100;
+	var startY = 70;
 	var spaceX = 85;
 	var spaceY = 85;
 	dots = game.add.group();
@@ -98,21 +102,20 @@ function create() {
 			}
 
 			var dot = dots.create(i*spaceX+offsetX, j*spaceY+startY, colors[game.rnd.integerInRange(0,colors.length-1)]+'Dot');
-			dot.row = j+1;
+			dot.row = j+1; //ad-hoc variable
 			dot.name = ((j+1)*i)+'dot';
 			dot.anchor.setTo(0.5,0.5);
 			dot.scale.setTo(0.4,0.4);
-			dot.physicsEnabled = true;
+			game.physics.p2.enable(dot, debug);
 			dot.body.setCircle(dot.width * .5);
-			dot.body.setMaterial(dotMaterial);
+			dot.body.mass = 100;
 			dot.body.setCollisionGroup(dotCG);				
-			dot.body.static = true;
+			dot.body.data.motionState = 2; //dot.body.static = true;
 			dot.body.collides(circleCG); //now it works!
 		}
 	}
 
-	game.physics.createContactMaterial(circleMaterial, dotMaterial, { friction: 0.04, restitution: 0.6 });
-	circle.body.collides([dotCG], function(circleBody,otherBody) {
+	circle.body.collides(dotCG, function(circleBody,otherBody) {
 		if (circle.collidedWith.indexOf(otherBody) == -1) {
 			circle.collidedWith.push(otherBody);
 			var row = otherBody.sprite.row;
@@ -128,68 +131,79 @@ function create() {
 	});
 	
 	//baskets
-	startX = 50;
-	startY = game.world.height - 35.0;
+	startX = 3;
+	startY = game.world.height - 70;
 	spaceX = 92.5;
 	var basketNumber = 11;
 	
-	basketMaterial = game.physics.createMaterial('basketMaterial');
-	basketCG = game.physics.createCollisionGroup();	
+	basketCG = game.physics.p2.createCollisionGroup();	
 	
 	baskets = game.add.group();
-	baskets.name = 'baskets';
+	baskets.name = 'baskets';	
 	for(var i=0; i<basketNumber; i++) {
 		var basket = baskets.create(i*spaceX+startX, startY, basketColors[game.rnd.integerInRange(0,basketColors.length-1)]);
 		basket.name = (i+1)+'basket';
 		basket.anchor.setTo(0.5,1.0);
 		basket.scale.setTo(0.75,0.75);
-		basket.physicsEnabled = true;
-		//basket.body.setPolygon(0,0, 60,0, 100,40, 60,80, 0,80);
-		basket.body.setMaterial(basketMaterial);
+		game.physics.p2.enable(basket, debug);
+		basket.body.clearShapes();
+		//basket.body.addPolygon({optimalDecomp: false, skipSimpleCheck: true, removeCollinearPoints: false}, [[0,5], [92,5], [82,70], [8,70]]);
+		
+		basket.body.addPolygon({optimalDecomp: false, skipSimpleCheck: true, removeCollinearPoints: false}, 
+		   [
+			   [0,0], [1,0], [15,60], [77,60], [91,0], [92,0], [82,70], [8,70]
+		   ]);
+		
+		basket.body.mass = 100;
 		basket.body.setCollisionGroup(basketCG);
-		basket.body.static = true;
+		basket.body.data.motionState = 2; //basket.body.static = true;
 		basket.body.collides(circleCG);
 	}
 	
-	game.physics.createContactMaterial(circleMaterial, basketMaterial, { friction: 0.04, restitution: 0.6 });
-	circle.body.collides([basketCG], function(circleBody, otherBody) {
-		var row = otherBody.sprite.row;
+	circle.body.collides(basketCG, function(circleBody, otherBody) {
 		switch(otherBody.sprite.key) {
-			case 'boxgreen':
-				points.p=parseInt(points.p*1.5);
+			case 'basketgreen':
+				console.log("green");
+				points.p=parseInt(points.p)*1.5;
 			break;				
-			case 'boxred':
-				points.p=parseInt(points.p*0.5);
+			case 'basketred':
+				console.log("red");
+				points.p=parseInt(points.p)*0.5;
 			break;				
 		}
 	});
+	
+    //  This part is vital if you want the objects with their own collision groups to still collide with the world bounds
+    //  (which we do) - what this does is adjust the bounds to use its own collision group.
+    game.physics.p2.updateBoundsCollisionGroup();	
+	
+	//Turn on impact events for the world, without this we get no collision callbacks
+	game.physics.p2.setImpactEvents(true);	
 }
 
-var started = false;
 function update() {
-	if (circle.body.static) {
-		circle.body.x = game.input.activePointer.worldX;
+	var speed = 7.5;
+	
+	if (game.input.keyboard.isDown(Phaser.Keyboard.CONTROL)) {
+		circle.body.data.motionState = 1;
 	}
-
-	if (game.input.activePointer.isDown) {
-		circle.body.static = false;
+	
+	if (circle.body.data.motionState == 2) {
+		if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+			circle.body.x-= speed;
+		}
+		else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+			circle.body.x+= speed;
+		}
+		
+		if (circle.body.x < 0 + circle.width * 0.5) {
+			circle.body.x = circle.width * 0.5;
+		} 
+		else if (circle.body.x > game.world.width - circle.width * 0.5) {
+			circle.body.x = game.world.width - circle.width * 0.5;
+		}
 	}
 }
 
 function render() {
-	/*
-	if (debug) {		
-		game.debug.renderPhysicsBody(circle.body);
-		if (dots != undefined) {
-			dots.forEach(function(dot) {
-				game.debug.renderPhysicsBody(dot.body);
-			});
-		}
-		if (baskets != undefined) {
-			baskets.forEach(function(basket) {
-				game.debug.renderPhysicsBody(basket.body);
-			});	
-		}
-	}
-	*/
 }
